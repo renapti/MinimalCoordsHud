e6cb9668929d312c78f66439e2e10800c33b1289
Ian <115433521+renapti@users.noreply.github.com>
2023-11-07 22:49:23 -0800

    Messy prototype update
    * Coordinates now show a "facing" indicator, a (+) or (-) showing which direction you're facing
    * Coordinates can now be anchored to any corner of the window, which is still very buggy and not at all finished
    * Much code needs reworking and / or cleaning up

--- src/main/java/com/renapti/minimalcoordshud/mixin/InGameHudMixin.java
+++ src/main/java/com/renapti/minimalcoordshud/mixin/InGameHudMixin.java
@@ -5,7 +5,11 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,21 +22,49 @@ import java.text.DecimalFormat;
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin
 {
-    @Unique
-    private final MinecraftClient mc = MinecraftClient.getInstance();
-
     @Shadow
     public abstract TextRenderer getTextRenderer();
 
     @Shadow
     protected abstract PlayerEntity getCameraPlayer();
 
+    @Shadow @Final
+    private MinecraftClient client;
+
+    @Shadow private int scaledHeight;
+    @Shadow private int scaledWidth;
+    // Colors
+    @Unique
+    final int GREEN = 0x98ffad;
+    @Unique
+    final int RED = 0xff98aa;
+    @Unique
+    final int WHITE = 0xffffff;
+    @Unique
+    final int BLACK = 0x000000;
+
+    // TODO: Is HUD RTL?
+    @Unique
+    boolean isRTL = false;
+
+    // TODO: Is HUD bottom-to-top?
+    @Unique
+    boolean isFromBottom = false;
+
+    // Indicators
+    @Unique
+    final private Text positive = Text.translatable("hud.minimalcoordshud.facing.positive");
+
+    @Unique
+    final private Text negative = Text.translatable("hud.minimalcoordshud.facing.negative");
+
 	// Render inject to draw text on the game HUD
+    // TODO: Configurable anchoring, positioning, linespacing, etc.
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderAutosaveIndicator(Lnet/minecraft/client/gui/DrawContext;)V"), method = "render")
     private void render(DrawContext context, float tickDelta, CallbackInfo ci)
     {
 		// Only show if HUD is not hidden, debug is not enabled, and mod is toggled on
-        if (mc.options.hudHidden || mc.options.debugEnabled || !MinimalCoordsHud.isHUDToggled)
+        if (client.options.hudHidden || client.options.debugEnabled || !MinimalCoordsHud.isHUDToggled)
             return;
 
         PlayerEntity player = getCameraPlayer();
@@ -44,19 +76,62 @@ public abstract class InGameHudMixin
         TextRenderer textRenderer = getTextRenderer();
 
 		// Format to two decimal digits
-        DecimalFormat df = new DecimalFormat("0.##");
+        DecimalFormat formatXZ = new DecimalFormat("0.00");
+        DecimalFormat formatY  = new DecimalFormat("0.##");
 
-		// Position of the text, anchored to top left
-        int xOffset = 5;
-        int yOffset = 5;
+		// Position of the text, anchored to top left, in pixels
+        // TODO: Fix offsets
+        int xOffset = (isRTL ? scaledWidth - 100 : 5);
+        int yOffset = (isFromBottom ? scaledHeight - 100 : 5);
 
-		// Spacing between the lines
+        // Coordinate lines
+        Text xT = Text.of("x: " + formatXZ.format(x));
+        Text yT = Text.of("y: " + formatY.format(y));
+        Text zT = Text.of("z: " + formatXZ.format(z));
+
+        // Offset of facing indicators
+        // TODO: Fix these offsets too
+        int xSignOffset = (isRTL ? (textRenderer.getWidth(xT.getString()) + textRenderer.getWidth(positive) + 4) * -1 :
+                textRenderer.getWidth(xT.getString()) + 4);
+        int zSignOffset = (isRTL ? (textRenderer.getWidth(zT.getString()) + textRenderer.getWidth(positive) + 4) * -1 :
+                textRenderer.getWidth(zT.getString()) + 4);
+
+		// Spacing between the lines in pixels
         int lineSpacing = 10;
 
-		// TODO: Configurable anchoring, positioning, linespacing, etc.
-        drawTextOutlined(context, textRenderer, "x: " + df.format(x), xOffset, yOffset, 0xffffff, 0x000000, true);
-        drawTextOutlined(context, textRenderer, "y: " + df.format(y), xOffset, yOffset + lineSpacing, 0xffffff, 0x000000, true);
-        drawTextOutlined(context, textRenderer, "z: " + df.format(z), xOffset, yOffset + lineSpacing * 2, 0xffffff, 0x000000, true);
+        drawTextOutlined(context, textRenderer, xT, xOffset, yOffset, WHITE, BLACK, true);
+        drawTextOutlined(context, textRenderer, yT, xOffset, yOffset + lineSpacing, WHITE, BLACK, true);
+        drawTextOutlined(context, textRenderer, zT, xOffset, yOffset + lineSpacing * 2, WHITE, BLACK, true);
+
+        // Player direction indicators
+        String facing = player.getHorizontalFacing().getName();
+        switch (facing)
+        {
+            // Facing +X
+            case "east" ->
+            {
+                drawTextOutlined(context, textRenderer, positive,
+                        xOffset + xSignOffset, yOffset, GREEN, BLACK, true);
+            }
+            // Facing +Z
+            case "south" ->
+            {
+                drawTextOutlined(context, textRenderer, positive,
+                        xOffset + zSignOffset, yOffset + lineSpacing * 2, GREEN, BLACK, true);
+            }
+            // Facing -X
+            case "west" ->
+            {
+                drawTextOutlined(context, textRenderer, negative,
+                        xOffset + xSignOffset, yOffset, RED, BLACK, true);
+            }
+            // Facing -Z
+            case "north" ->
+            {
+                drawTextOutlined(context, textRenderer, negative,
+                        xOffset + zSignOffset, yOffset + lineSpacing * 2, RED, BLACK, true);
+            }
+        }
     }
 
 	/**
@@ -72,7 +147,7 @@ public abstract class InGameHudMixin
 	 * @param thick        Whether or not the outline should be 4-pixel-connected or 8-pixel-connected.
 	 */
     @Unique
-    private void drawTextOutlined(DrawContext context, TextRenderer textRenderer, String message, int x, int y, int color, int outlineColor, boolean thick)
+    private void drawTextOutlined(DrawContext context, TextRenderer textRenderer, Text message, int x, int y, int color, int outlineColor, boolean thick)
     {
 		// Draw 4-pixel-connected outline
         context.drawText(textRenderer, message, x - 1, y, outlineColor, false);
